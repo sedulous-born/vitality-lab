ba01532423e579e10cf2116dc31136c4706f71b1
Naga Malleshwar Reddy Lingala <malleshgradu@gmail.com>
2025-07-24 17:25:13 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/vitality-lab">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/research" element={<Research />} />
